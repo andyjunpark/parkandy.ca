6434827c70b61c8679b76023eaecd5eb89323c44
Andy Junhyuk Park <34406299+pos03142ap@users.noreply.github.com>
2021-10-08 14:51:08 -0700

    Added scroll stop until loading

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,13 @@
 const loader = document.querySelector('.loader');
 const main = document.querySelector('.video-wrap');
+const body = document.querySelector('body');
 
 // Fade in Video Wrapper after Loader //
 function init() {
     setTimeout(() => {
         loader.style.opacity = 0;
         loader.style.display = 'none';
+        body.style.overflow ='visible';
 
         main.style.display ='block';
         setTimeout(() => (main.style.opacity = 1), 50);
@@ -13,3 +15,4 @@ function init() {
 }
 
 init();
+
